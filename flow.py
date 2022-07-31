8532021a1145821c9ae865a59ac005c9d05d23f7
Stéphan Taljaard <steptaljaard@gmail.com>
2022-07-31 21:17:23 +0200

    Add basic script

---
+++ flow.py
@@ -0,0 +1,75 @@
+import base64
+import io
+
+import requests
+from PIL import Image
+
+
+def get_prompt() -> str:
+    return "chocolate toad"
+
+
+def perform_request(prompt: str) -> requests.Response:
+    print("Starting request")
+    headers = {
+        "authority": "backend.craiyon.com",
+        "accept": "application/json",
+        "accept-language": "en-ZA,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,af;q=0.6,es;q=0.5",
+        # Already added when you pass json=
+        # 'content-type': 'application/json',
+        "origin": "https://www.craiyon.com",
+        "sec-ch-ua": '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+    }
+
+    json_data = {"prompt": prompt}
+
+    post = requests.post("https://backend.craiyon.com/generate", headers=headers, json=json_data)
+    print("Request done")
+    return post
+
+
+def concat_images(images: [bytes]) -> Image:
+    # Open images and resize them
+
+    width, height = images[0].size  # Assume all images have the same size
+    shape = (3, 3)
+
+    combined_image_size = width * shape[1], height * shape[0]
+
+    image = Image.new("RGB", combined_image_size)
+    for row in range(shape[0]):
+        for col in range(shape[1]):
+            offset = width * col, height * row
+            idx = row * shape[1] + col
+            image.paste(images[idx], offset)
+    return image
+
+
+def combine_images(response_json: dict):
+    # Convert bytes to images
+    images_bytes = [base64.b64decode(image) for image in response_json["images"]]
+    images = [Image.open(io.BytesIO(b)) for b in images_bytes]
+
+    # Create and save image grid
+    image = concat_images(images)
+    image.save("image.png", "PNG")
+
+
+def main():
+    prompt = get_prompt()
+    response = perform_request(prompt)
+
+    if response.status_code == 200:
+        combine_images(response.json())
+
+    else:
+        print(f"Bad response: {response.status_code}")
+
+
+main()
