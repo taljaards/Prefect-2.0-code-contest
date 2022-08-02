5cf43945f278ba4e665979d17aa734a921d247f0
Stéphan Taljaard <steptaljaard@gmail.com>
2022-08-02 20:17:56 +0200

    Change request handling

--- flow.py
+++ flow.py
@@ -63,7 +63,10 @@ def perform_request(prompt: str) -> requests.Response:
 
 
 @task
-def get_images_from_response(response: requests.Response) -> list[Image]:
+def get_images_from_response(response: requests.Response) -> Union[None, list[Image]]:
+    if response.status_code != 200:
+        return None
+
     # Convert bytes to images
     images_bytes = [base64.b64decode(image) for image in response.json()["images"]]
     return [Image.open(io.BytesIO(b)) for b in images_bytes]
@@ -131,8 +134,8 @@ def main():
     prompt = get_prompt()
     response = perform_request(prompt)
 
-    if response.status_code == 200:
-        images = get_images_from_response(response)
+    images = get_images_from_response(response)
+    if images:
         image = combine_images(images)
         image = add_border_and_prompt(image, prompt)
         save_image(image, prompt)
